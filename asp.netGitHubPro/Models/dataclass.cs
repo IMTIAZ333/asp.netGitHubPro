aff854e562ed089058225b3c2b9461debbd7fe8d
imtiaz333 <imtiazkhan607@gmail.com>
2026-06-22 15:52:08 +0500

    added name, address , id, propertes to class

--- asp.netGitHubPro/Models/dataclass.cs
+++ asp.netGitHubPro/Models/dataclass.cs
@@ -1,7 +1,12 @@
-﻿namespace asp.netGitHubPro.Models
+﻿using System.Security.Principal;
+
+namespace asp.netGitHubPro.Models
 {
     public class dataclass
     {
-     public string name {get;set}
+        public int id   { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+
     }
 }
