3398b693daafa0fa1fde44491fdd88f9a0005aab
imtiazpc <imtiazkhan607@gmail.com>
2026-06-22 16:20:22 +0500

    added property to dataclass

--- asp.netGitHubPro/Models/dataclass.cs
+++ asp.netGitHubPro/Models/dataclass.cs
@@ -4,7 +4,10 @@ namespace asp.netGitHubPro.Models
 {
     public class dataclass
     {
-     public string name {get;set}
-        public string address {get;set}
+     public string name { get; set; }
+        public string address { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
     }
 }
