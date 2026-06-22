058f3a3c5e5eb02708db56c012fe743e299dd6aa
imtiaz333 <imtiazkhan607@gmail.com>
2026-06-22 16:06:28 +0500

    change resversd

--- asp.netGitHubPro/Models/dataclass.cs
+++ asp.netGitHubPro/Models/dataclass.cs
@@ -1,7 +1,15 @@
-﻿namespace asp.netGitHubPro.Models
+﻿using System.Security.Principal;
+
+namespace asp.netGitHubPro.Models
 {
     public class dataclass
     {
-     public string name {get;set}
+<<<<<<< HEAD
+        public int id   { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+
+=======
+>>>>>>> parent of 865e9ff (add new property to class)
     }
 }
